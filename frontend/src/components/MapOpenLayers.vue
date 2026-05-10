687959d2cbab08f097086b411d51d25f09898365
Annika Terhörst <annika.terhoerst@gmail.com>
2026-05-10 22:07:13 +0200

    small changes (comments, icon, ...)

--- frontend/src/components/MapOpenLayers.vue
+++ frontend/src/components/MapOpenLayers.vue
@@ -11,12 +11,14 @@ import OSM from 'ol/source/OSM'
 import { fromLonLat } from 'ol/proj'
 import 'ol/ol.css'
 
-const mapContainer = ref(null)
-let map = null
+const mapContainer = ref(null) // reactive container for the map div
+let map = null // variable to hold the OpenLayers map instance
 
-// Münster coordinates [longitude, latitude]
+// Münster coordinates [longitude, latitude], converted to the map's projection
 const MUENSTER_COORDS = fromLonLat([7.6261, 51.9607])
 
+// Runs after Vue has rendered the template and the div actually exists in the DOM
+// Create the OpenLayers map and attach it to the div
 onMounted(() => {
   map = new Map({
     target: mapContainer.value,
@@ -32,6 +34,8 @@ onMounted(() => {
   })
 })
 
+// Runs when the component is removed from the page 
+// Skipping this could cause memory leaks if the map component gets mounted and unmounted multiple times during navigation
 onUnmounted(() => {
   if (map) {
     map.setTarget(null)
@@ -40,6 +44,7 @@ onUnmounted(() => {
 })
 </script>
 
+<!-- Size of the map container -->
 <style scoped>
 .map-container {
   width: 100%;
